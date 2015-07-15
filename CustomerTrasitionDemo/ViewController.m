ba1e8c0cdedf86a7bdb5c85ec27f771f61e8f720
赛峰 施 <saifengshi1@saifengdeMacBook-Pro.local>
2015-07-16 00:20:32 +0800

    增加了uiview的2个block动画：关键帧动画，弹簧动画

--- CustomerTrasitionDemo/ViewController.m
+++ CustomerTrasitionDemo/ViewController.m
@@ -10,13 +10,42 @@
 #import "FirstCustomSegueUnwind.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *browView;
+@property (weak, nonatomic) IBOutlet UIView *blueView;
 @end
 
 @implementation ViewController
 
 - (IBAction)returnFromSegueActions:(UIStoryboardSegue *)sender {
     
+}
+
+- (void)viewDidLoad {
+    self.browView.exclusiveTouch = YES;
+    [UIView animateKeyframesWithDuration:5.0 delay:0 options:UIViewKeyframeAnimationOptionAutoreverse animations:^{
+        [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.2 animations:^{
+            self.browView.frame = CGRectOffset(self.browView.frame, 15, 5);
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.2 relativeDuration:0.2 animations:^{
+            self.browView.frame = CGRectOffset(self.browView.frame, 15, -5);
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.4 relativeDuration:0.2 animations:^{
+            self.browView.frame = CGRectOffset(self.browView.frame, 15, 5);
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.6 relativeDuration:0.2 animations:^{
+            self.browView.frame = CGRectOffset(self.browView.frame, 15, -5);
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{
+            self.browView.frame = CGRectOffset(self.browView.frame, 15, 5);
+        }];
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    [UIView animateWithDuration:5 delay:0 usingSpringWithDamping:0.1 initialSpringVelocity:0.8 options:UIViewAnimationOptionCurveEaseIn animations:^{
+//        self.blueView.frame = CGRectOffset(self.blueView.frame, 0, 30);
+        self.blueView.backgroundColor = [UIColor greenColor];
+    } completion:NULL];
 }
 
 - (UIStoryboardSegue *)segueForUnwindingToViewController:(UIViewController *)toViewController fromViewController:(UIViewController *)fromViewController identifier:(NSString *)identifier {
